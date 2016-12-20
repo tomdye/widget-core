f43f370b2f53868179416d580b08fcba72d0ed3f
Tom Dye <github@tomdye.co.uk>
2016-12-20 15:40:29 +0000

    got gruntfile working

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,55 +4,34 @@ module.exports = function (grunt) {
 
 	require('grunt-dojo2').initConfig(grunt, {
 		stylus: {
-			dev: {
-				options: {
-					'include css': true
-				},
-				files: [ {
-					expand: true,
-					src: 'src/**/*.styl',
-					ext: '.css'
-				} ]
-			},
 			dist: {
-				options: {
-					relativeDest: '/dist/umd'
-				},
 				files: [ {
 					expand: true,
-					src: 'src/**/*.styl',
-					ext: '.css'
+					src: '**/*.styl',
+					cwd: 'src',
+					ext: '.module.css',
+					dest: 'dist/umd/'
 				}]
 			}
+		},
+		postcss: {
+			options: {
+				map: true,
+				processors: [
+					require('postcss-modules')({
+						generateScopedName: '[name]__[local]__[hash:base64:5]'
+					})
+				]
+			},
+			dist: {
+				files: [ {
+					expand: true,
+					src: 'dist/umd/**/*.css',
+					ext: '.module.css'
+				} ]
+			}
 		}
-		// postcss: {
-		// 	options: {
-		// 		map: true,
-		// 		processors: [
-		// 			require('postcss-modules')
-		// 		]
-		// 	},
-		// 	dev: {
-		// 		files: [ {
-		// 			expand: true,
-		// 			flatten: true,
-		// 			src: 'src/**/*.css',
-		// 			ext: '.css',
-		// 			dest: 'src/themes/structural/_generated/'
-		// 		} ]
-		// 	}
-		// }
 	});
-
-	grunt.registerTask('dev', [
-		'clean:typings',
-		'typings:dev',
-		'tslint',
-		'clean:dev',
-		'ts:dev',
-		'stylus:dev',
-		'copy:staticTestFiles'
-	]);
 
 	grunt.registerTask('dist', [
 		'clean:typings',
@@ -62,60 +41,7 @@ module.exports = function (grunt) {
 		'copy:staticDefinitionFiles',
 		'ts:dist',
 		'stylus:dist',
+		'postcss:dist',
 		'fixSourceMaps'
 	]);
 };
-
-
-// // var path = require('path');
-// // var fs = require('fs');
-
-// module.exports = function (grunt) {
-// 	grunt.loadNpmTasks('grunt-contrib-stylus');
-// 	grunt.loadNpmTasks('grunt-postcss');
-
-// 	var staticExampleFiles = [ 'src/examples/**', '!src/examples/**/*.js' ];
-
-// 	require('grunt-dojo2').initConfig(grunt, {
-// 		copy: {
-// 			staticExampleFiles: {
-// 				expand: true,
-// 				cwd: '.',
-// 				src: staticExampleFiles,
-// 				dest: '<%= devDirectory %>'
-// 			}
-// 		},
-// 		dtsGenerator: {
-// 			options: {
-// 				main: 'dojo-widgets/main'
-// 			}
-// 		},
-// 		stylus: {
-// 			dev: {
-// 				options: {
-// 					'include css': true
-// 				},
-// 				files: [ {
-// 					expand: true,
-// 					src: 'src/themes/**/*.styl',
-// 					ext: '.css',
-// 					dest: '_build/'
-// 				} ]
-// 			},
-// 			dist: {
-// 				options: {},
-// 				files: [ {
-// 					expand: true,
-// 					cwd: 'src/',
-// 					src: 'themes/**/*.styl',
-// 					ext: '.css',
-// 					dest: 'dist/'
-// 				}]
-// 			}
-// 		},
-
-
-// 	});
-
-
-// };
