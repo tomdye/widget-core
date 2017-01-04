75f6c05ae9dc5d037175993aeca4c805d19303e3
Tom Dye <tom@Toms-MacBook-Pro.local>
2017-01-04 14:26:18 +0000

    working with theme manager

--- src/components/tabpanel/createTabPanel.ts
+++ src/components/tabpanel/createTabPanel.ts
@@ -3,31 +3,79 @@ import { Widget, WidgetOptions, WidgetProperties, WidgetState, DNode } from '../
 import createWidgetBase from '../../createWidgetBase';
 import { v } from '../../d';
 import * as css from './tabpanel.module.styl';
-import themeManager from '../../themeManager';
+// import * as theme from '../../themes/default/tabpanel.module.styl';
+import themeManager, { Theme } from '../../themeManager';
 
 export interface TabPanelState extends WidgetState {}
 
-export interface TabPanelOptions extends WidgetOptions<TabPanelState, WidgetProperties> {}
+export interface TabPanelProperties extends WidgetProperties {
+	overrideClasses: Theme;
+}
 
-export type TabPanel = Widget<TabPanelState, WidgetProperties>;
+export interface TabPanelOptions extends WidgetOptions<TabPanelState, TabPanelProperties> {}
+
+export type TabPanel = Widget<TabPanelState, TabPanelProperties>;
 
 export interface TabPanelFactory extends ComposeFactory<TabPanel, TabPanelOptions> {}
+
+// export type ActiveClassMap<T> = {
+// 	[P in keyof T]?: ActiveClasses;
+// }
+
+// export type ActiveClasses = {
+// 	[key: string]: boolean;
+// }
+
+// export type ClassMap = {};
+
+// function getClasses<T, U, V>(baseClasses: T, themeClasses: U, overrideClasses?: V): ActiveClassMap<T> & ActiveClassMap<U> & ActiveClassMap<V> {
+// 	// create the class map to be returned
+// 	const activeClassMap: ActiveClassMap<T & U & V> = <ActiveClassMap<T & U & V>> {};
+
+// 	// create array of class lists and push on overrideClasses if it exists
+// 	const classLists: (T | U | V)[] = [ baseClasses, themeClasses ];
+// 	overrideClasses && classLists.push(overrideClasses);
+
+// 	// get the combined classnames from all three
+// 	const classNames = Object.keys(Object.assign({}, baseClasses, themeClasses, overrideClasses));
+
+// 	// loop through the class names
+// 	classNames.forEach((className) => {
+// 		// create an empty class map for the given classname
+// 		// ie.
+// 		// { className1: { css-classname1: true, theme-module-classname1: true } }
+// 		//
+
+// 		const classMap: ActiveClasses = activeClassMap[<keyof T & U & V> className] = {};
+
+// 		// loop through classlists and build up the class map
+// 		classLists.forEach((classList) => {
+// 			if (classList.hasOwnProperty(className)) {
+// 				const generatedClassName = (<any> classList)[className];
+// 				classMap[generatedClassName] = true;
+// 			}
+// 		});
+// 	});
+
+// 	return activeClassMap;
+// }
 
 const createTabPanel: TabPanelFactory = createWidgetBase.mixin({
 	mixin: {
 		tagName: 'tab-panel',
-		classes: [ css.root ],
+		classes: [ css.tabPanel ],
 		getChildrenNodes: function (this: TabPanel): DNode[] {
-			const theme: any = themeManager.theme;
+			const { overrideClasses } = this.properties;
+			const themeable = themeManager.getThemeClasses(overrideClasses);
 
 			return [
-				v(`ul.${css.tabs}.${theme.tabPanelTabs}`, [
+				v(`ul`, { classes: { [ css.tabPanelTabs ]: true, ...themeable.tabPanelTabs } }, [
 					v('li', [ 'tab1' ]),
-					v(`li.${css.activeTab}.${theme.tabPanelActiveTab}`, [ 'tab2' ]),
+					v('li', { classes: { [ css.tabPanelActiveTab ]: true, ...themeable.tabPanelActiveTab } }, [ 'tab2' ]),
 					v('li', [ 'tab3' ])
 				]),
-				v(`div.${css.panels}.${theme.tabPanelPanels}`, [
-					v(`div.${css.panel}.${theme.tabPanelPanel}`, [ 'hello world' ])
+				v('div', { classes: { [ css.tabPanelPanels ]: true, ...themeable.tabPanelPanels } }, [
+					v('div', { classes: { [ css.tabPanelPanel ]: true, ...themeable.tabPanelPanel } }, [ 'hello world' ])
 				])
 			];
 		}
--- src/themeManager.ts
+++ src/themeManager.ts
@@ -1,18 +1,49 @@
 import * as defaultTheme from './themes/default/theme.module.styl';
 import { assign } from 'dojo-core/lang';
 
+export type ActiveClassMap<T> = {
+	[P in keyof T]?: ActiveClasses;
+}
+
+export type ActiveClasses = {
+	[key: string]: boolean;
+}
+
+export type Theme = {
+	[key: string]: boolean;
+}
+
+function addClassNameToMap(classMap: ActiveClasses, classList: {}, className: string) {
+	if (classList && classList.hasOwnProperty(className)) {
+		const generatedClassName = (<any> classList)[className];
+		classMap[generatedClassName] = true;
+	}
+}
+
+function getClasses<T>(themeClasses: T, overrideClasses: {}): ActiveClassMap<T> {
+	// create the class map to be returned
+	const activeClassMap: ActiveClassMap<T> = <ActiveClassMap<T>> {};
+
+	// loop through the class names
+	Object.keys(themeClasses).forEach((className) => {
+		const classMap: ActiveClasses = activeClassMap[<keyof T> className] = {};
+
+		addClassNameToMap(classMap, themeClasses, className);
+		addClassNameToMap(classMap, overrideClasses, className);
+	});
+
+	return activeClassMap;
+}
+
 export class ThemeManager  {
 	private _theme = defaultTheme;
 
-	constructor() {
-		// this._theme = defaultTheme;
-	};
 	set theme(theme: {}) {
-		console.log('theme set');
 		this._theme = assign(this._theme, theme);
-	};
-	get theme() {
-		return this._theme;
+	}
+
+	getThemeClasses(overrideClasses: {}) {
+		return getClasses(this._theme, overrideClasses);
 	}
 };
 
